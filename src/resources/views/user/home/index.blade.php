efe1860c13bd7058e835e162f452c63ffdfa84f2
okajima <r_okajima@lermo.co.jp>
2025-09-25 19:19:32 +0900

    カード レイアウト修正

--- src/resources/views/user/home/index.blade.php
+++ src/resources/views/user/home/index.blade.php
@@ -12,36 +12,44 @@
                     <h2>機能</h2>
                     <ul class="home-main-content-product-list">
                         @foreach ($product_function as $product)
-                            <li class="home-main-content-product-item">
+                            <div class="card w-50">
                                 <form class="api-action" action="{{ route('user.api.cart_item.add') }}" method="POST">
                                     @csrf
                                     <input type="hidden" name="product_id" value="{{ $product->id }}">
                                     <input type="hidden" name="quantity" value="1">
-                                    <img alt="画像" src="https://img2.lancers.jp/portfolio/598448/1770412/895176994898f838de3b1e69b7f6c0c6a77eacb6a1f955e3d77667558a97d7ee/32420704_400_0.jpg" />
-                                    <p class="name">{{ $product->name }}</p>
-                                    <p class="price">{{ $product->price }}円</p>
-                                    <button type="submit">+ カートに追加</button>
+                                    <div class="card-body">
+                                        <h5 class="card-title">{{ $product->name }}</h5>
+                                        <p class="card-text">{{ $product->price }}円</p>
+                                        <button type="submit" class="btn btn-primary">+ カートに追加</button>
+                                    </div>
                                 </form>
+                            </div>
                         @endforeach
                     </ul>
-                    <a href="" class="more-link">もっと見る ＞</a>
+                    <a href="" class="more-link">もっと見る ></a>
                 </div>
             </section>
-            <section class="home-main-content">
+            {{-- <section class="home-main-content">
                 <div class="home-main-content-wrap">
                     <h2>サービス</h2>
                     <ul class="home-main-content-product-list">
                         @foreach ($product_service as $product)
-                            <li class="home-main-content-product-item">
-                                <img alt="画像" src="https://img2.lancers.jp/portfolio/598448/1770412/895176994898f838de3b1e69b7f6c0c6a77eacb6a1f955e3d77667558a97d7ee/32420704_400_0.jpg" />
-                                <p class="name">{{ $product->name }}</p>
-                                <p class="price">{{ $product->price }}円</p>
-                                <button>+ カートに追加</button>
-                            </li>
+                            <div class="card w-50">
+                                <form class="api-action" action="{{ route('user.api.cart_item.add') }}" method="POST">
+                                    @csrf
+                                    <input type="hidden" name="product_id" value="{{ $product->id }}">
+                                    <input type="hidden" name="quantity" value="1">
+                                    <div class="card-body">
+                                        <h5 class="card-title">{{ $product->name }}</h5>
+                                        <p class="card-text">{{ $product->price }}円</p>
+                                        <button type="submit" class="btn btn-primary">+ カートに追加</button>
+                                    </div>
+                                </form>
+                            </div>
                         @endforeach
                     </ul>
                 </div>
-            </section>
+            </section> --}}
         </div>
     </main>
 </x-user.common>
--- src/resources/views/user/shopping/complete.blade.php
+++ src/resources/views/user/shopping/complete.blade.php
@@ -2,6 +2,11 @@
     <main class="user-main">
         <div class="user-container">
             <h1 class="shopping-title">購入完了</h1>
+            <p>
+                ご購入いただきありがとうございます。<br>
+                ご登録のメールアドレスに、ご利用開始のご案内をお送りしました。<br>
+                届かない場合は迷惑メールフォルダもご確認ください。
+            </p>
         </div>
     </main>
 </x-user.common>
